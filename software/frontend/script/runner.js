870db876c676ac99f0ae1e7f4d282961e9cdbed3
mameaw14 <pinyapornster@gmail.com>
2016-08-03 18:14:24 +0700

    month format

--- software/frontend/script/runner.js
+++ software/frontend/script/runner.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
     var timeLabel = $('#time-label');
     var obj = null;
-
+    var mL = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var mS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
     setInterval(function() {
         // senter form {"temp":(), "enter":(),"leave":()}
 
@@ -14,7 +15,7 @@ $(document).ready(function() {
 
         // time
         var date = new Date();
-        timeLabel.text(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "   " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+        timeLabel.text(date.getDate() + " " + mS[date.getMonth()] + " " + date.getFullYear() + "   " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
     }, 1000);
 
 });
