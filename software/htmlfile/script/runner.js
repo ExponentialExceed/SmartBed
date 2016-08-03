7eeac86da50e38a9cba29ddb99935572b8efb176
kamontat <kamontat_c@hotmail.com>
2016-08-03 11:33:50 +0700

    fixed some bug

--- software/htmlfile/script/runner.js
+++ software/htmlfile/script/runner.js
@@ -1,20 +1,20 @@
-var timeLabel = $('#time-label');
+$(document).ready(function() {
 
-var obj = null;
+    var timeLabel = $('#time-label');
+    var obj = null;
 
-setInterval(function() {
-    // senter form {"temp":(), "enter":(),"leave":()}
+    setInterval(function() {
+        // senter form {"temp":(), "enter":(),"leave":()}
 
-    // server
-    $.ajax({url: 'http://10.32.176.4/Exponential'}).done(function(data) {
-        // TODO: add information to web
-        obj = JSON.parse(data);
-        updateSeat();
-        temperatureLabel.text(obj.temp + "°C");
-    });
+        // server
+        // $.ajax({url: 'http://10.32.176.4/Exponential'}).done(function(data) {
+        //     // TODO: add information to web
+        //     obj = JSON.parse(data);
+        // });
 
-    // time
-    var date = new Date();
-    timeLabel.text("Time: " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
-    timeLabel.text("Date: " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear());
-}, 1000);
+        // time
+        var date = new Date();
+        timeLabel.text(date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + "   " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());
+    }, 1000);
+
+});
