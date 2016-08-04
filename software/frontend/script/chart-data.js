b64f618e80a4765262be900b674c5469822cbc20
kamontat <kamontat_c@hotmail.com>
2016-08-04 08:13:33 +0700

    fixed layout and bug

--- software/frontend/script/chart-data.js
+++ software/frontend/script/chart-data.js
@@ -1,36 +1,65 @@
 var warning = $('#warining-div');
 var randomScalingFactor = function() {
-    return Math.round(Math.random() * 1000)
+    return Math.round(Math.random() * 100)
 };
 var lineChartData = {
-    labels: ["Hour: 1", "Hour: 2", "Hour: 3", "Hour: 4", "Hour: 5", "Hour: 6", "Hour: 7", "Hour: 8", "Hour: 9", "Hour: 10", "Hour: 11", "Hour: 12"],
-    datasets: [{
-        label: "sleep",
-        fillColor: "rgba(48, 164, 255, 0.135)",
-        strokeColor: "rgba(48, 164, 255, 1)",
-        pointColor: "rgba(48, 164, 255, 1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(48, 164, 255, 1)",
-        data: [40,50,30,20,43,41,35,67,43,99,45,21]
-    }]
+    labels: [
+        "Hour: 1",
+        "Hour: 2",
+        "Hour: 3",
+        "Hour: 4",
+        "Hour: 5",
+        "Hour: 6",
+        "Hour: 7",
+        "Hour: 8",
+        "Hour: 9",
+        "Hour: 10",
+        "Hour: 11",
+        "Hour: 12"
+    ],
+    datasets: [
+        {
+            label: "sleep",
+            fillColor: "rgba(48, 164, 255, 0.135)",
+            strokeColor: "rgba(48, 164, 255, 1)",
+            pointColor: "rgba(48, 164, 255, 1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(48, 164, 255, 1)",
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }
+    ]
 };
-
 
 var setdata = function(obj, value) {
     var date = new Date();
 
     lineChartData.labels = countHour(value);
-    lineChartData.datasets = [{
-        label: "Sleep",
-        fillColor: "rgba(48, 164, 255, 0.135)",
-        strokeColor: "rgba(48, 164, 255, 1)",
-        pointColor: "rgba(48, 164, 255, 1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(48, 164, 255, 1)",
-        data: obj
-    }];
+    lineChartData.datasets = [
+        {
+            label: "Sleep",
+            fillColor: "rgba(48, 164, 255, 0.135)",
+            strokeColor: "rgba(48, 164, 255, 1)",
+            pointColor: "rgba(48, 164, 255, 1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(48, 164, 255, 1)",
+            data: obj
+        }
+    ];
 
     warning.text(calWarning(obj));
 }
@@ -60,13 +89,9 @@ var countHour = function(value) {
 var set = function(obj, value) {
     setdata(obj, value);
     var chart1 = document.getElementById("line-chart").getContext("2d");
-    window.myLine = new Chart(chart1).Line(lineChartData, {
-        responsive: true
-    });
+    window.myLine = new Chart(chart1).Line(lineChartData, {responsive: true});
 }
 window.onload = function() {
     var chart1 = document.getElementById("line-chart").getContext("2d");
-    window.myLine = new Chart(chart1).Line(lineChartData, {
-        responsive: true
-    });
+    window.myLine = new Chart(chart1).Line(lineChartData, {responsive: true});
 };
